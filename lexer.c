6e9ca2d67e67b76fedcf16c90f49d29fb68034a7
Kartik Dang <f20200880@pilani.bits-pilani.ac.in>
2024-03-02 19:39:34 +0530

    Remove Comment Function Added

--- lexer.c
+++ lexer.c
@@ -862,6 +862,35 @@ struct tokenDetails *getNextToken(FILE *f)
 
 void removeComments(char *testcaseFile)
 {
+
+     FILE *srcFile = fopen(testcaseFile, "r");
+    FILE *tempFile = fopen("temp.txt", "w");
+
+    if (srcFile == NULL || tempFile == NULL) {
+        printf("Error opening file.\n");
+        exit(1);
+    }
+
+    char line[1024];
+    while (fgets(line, sizeof(line), srcFile) != NULL) {
+        char* commentStart = strchr(line, '%');
+        if (commentStart != NULL) {
+            
+            char* newlinePos = strchr(commentStart, '\n');
+            if (newlinePos != NULL) {
+                *commentStart = '\n';
+                *(commentStart + 1) = '\0';
+            } else {
+                *commentStart = '\0';
+            }
+        }
+        fputs(line, tempFile);
+    }
+
+    fclose(srcFile);
+    fclose(tempFile);
+
+    rename("temp.txt", "commentRemoval.txt");
 }
 
 int main()
