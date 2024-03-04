17ecdde2528cc526f0621b73487b613c43492d3a
Ayush-327 <ayush327@gmail.com>
2024-03-05 00:29:48 +0530

    Deleted Extra files

--- temporary.c
+++ temporary.c
@@ -2,8 +2,10 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include "parse_tree.h"
-// #include "stackImplementation.h"
-// #include "token.h"
+#include "stackImplementation.h"
+#include "token.h"
+
+
 int main(){
     NodeT* root = createRootNodeT();
     NodeT* n1 = createNodeT(2,root);
@@ -13,16 +15,16 @@ int main(){
     //printf("%d",root->terminal);
     // printf("%d\t%d",root->children[0]->name_rule,root->children[1]->name_rule);
     inorder(root);
-    // Stack* st = createStack();
-    // Node* st1 = createNode(root,st); 
-    // TdNode* ll = (TdNode*) malloc(sizeof(TdNode));
-    // //I need pareser table
-    // TdNode* prev = ll;
-    // TdNode* temp = ll;
-    // while(temp!=NULL){
-    //     char token_seen[50];
-    //     strcpy(token_seen, temp->tokenDet->token);
-    //     prev = temp;
-    //     temp = temp->next;
-    // }
+    Stack* st = createStack();
+    Node* st1 = createNode(root,st); 
+    TdNode* ll = (TdNode*) malloc(sizeof(TdNode));
+    //I need pareser table
+    TdNode* prev = ll;
+    TdNode* temp = ll;
+    while(temp!=NULL){
+        char token_seen[50];
+        strcpy(token_seen, temp->tokenDet->token);
+        prev = temp;
+        temp = temp->next;
+    }
 }
