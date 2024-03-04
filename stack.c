590466da44f24538c9aa302ab3bb5de92321da28
Ayush-327 <ayush327@gmail.com>
2024-03-04 15:36:21 +0530

    Stack Added

---
+++ stack.c
@@ -0,0 +1,58 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+struct tokenDetails
+{
+    char token[100];
+    char lexeme[100];
+    int lineNumber;
+    int value;
+    bool err;
+    char errMessage[100];
+};
+
+typedef struct LinkedList{
+    int value
+    struct LinkedList* next;
+} LinkedList;
+
+typedef struct Stack{
+    LinkedList* list;
+}
+
+LinkedList* allocate_list() {
+    LinkedList* newList = (LinkedList*) malloc(sizeof(LinkedList));
+    return newList;
+}
+
+Stack* allocate_stack(){
+    Stack* newStack = (Stack*) malloc(sizeof(Stack));
+    return newStack;
+}
+
+LinkedList* list_insert(LinkedList* list, int value) {
+    if(!list) {
+        LinkedList* head = allocate_list();
+        head->val = val;
+        head->next = NULL;
+        list = head;
+        return list;
+    }
+    else if(list->next == NULL){
+        LinkedList* node = allocate_list();
+        node->val = val;
+        node->next = NULL;
+        list->next = node;
+        return list;
+    }
+
+    LinkedList* temp = list;
+
+    while(temp->next->next) temp = temp->next;
+    LinkedList* node = allocate_list();
+    node->val = val;
+    node->next = NULL;
+    temp->next = node;
+    return list;
+}
+
