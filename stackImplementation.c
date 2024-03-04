294cd1eed17fb839f9d2b46a86dc66d9946ff813
Kartik Dang <f20200880@pilani.bits-pilani.ac.in>
2024-03-04 19:07:04 +0530

    Stack Implementation Updated

---
+++ stackImplementation.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Node
+{
+    int val;
+    struct Node *next;
+} Node;
+
+typedef struct Stack
+{
+    Node *head;
+} Stack;
+
+Stack *createStack()
+{
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
+    stack->head = NULL;
+    return stack;
+}
+
+Node *createNode(int val, Stack *stack)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->val = val;
+    newNode->next = stack->head;
+    return newNode;
+}
+
+void push(Stack *stack, int val)
+{
+    Node *newNode = createNode(val, stack);
+    stack->head = newNode;
+    // return newNode;
+}
+
+int pop(Stack *stack)
+{
+    if (stack->head == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        int val = stack->head->val;
+        Node *temp = stack->head;
+        stack->head = stack->head->next;
+        free(temp);
+        return val;
+    }
+}
+
+int peek(Stack *stack)
+{
+    if (stack->head == NULL)
+    {
+        return -1;
+    }else{
+        return stack->head->val;
+    }
+}
+
+void printStack(Stack* stack){
+    Node* temp = stack->head;
+    while(temp){
+        printf("%d\n", temp->val);
+        temp = temp->next;
+    }
+}
+
+int main(){
+    Stack* st = createStack();
+    push(st, 1);
+    push(st, 2);
+    push(st, 3);
+
+    printStack(st);
+    printf("\n%d\n\n",pop(st));
+    printStack(st);
+}
